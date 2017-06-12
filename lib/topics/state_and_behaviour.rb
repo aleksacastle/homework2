9ce260c98dc0d997e786ad8563d0f0bc00f39be9
aleksacastle <aaklochko@gmail.com>
2017-06-12 13:35:57 +0300

    changed according to sveredyuk comments

--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -6,26 +6,30 @@ module StateAndBehaviour
   class Car
     attr_reader :year, :color, :model, :current_speed
     
+    DEFAULT_COLOR = 'purple'
+ +  DEFAULT_MODEL = 'skyline'
+ +  DEFAULT_YEAR = 2015
+ +  DEFAULT_SPEED = 0
+    
     def self.default_car
-      car_attributes = {}
-      new(car_attributes)
+     new({})
     end
 
     def initialize(car_attributes)
-      raise ArgumentError unless car_attributes.is_a?(Hash)
+      raise ArgumentError unless car_attributes.instance_of?(Hash)
 
-      @year = car_attributes.fetch(:year, 2015)
-      @color = car_attributes.fetch(:color, 'purple')
-      @model = car_attributes.fetch(:model, 'skyline')
-      @current_speed = 0
+      @year = car_attributes.fetch(:year, DEFAULT_YEAR)
+      @color = car_attributes.fetch(:color, DEFAULT_COLOR)
+      @model = car_attributes.fetch(:model, DEFAULT_MODEL)
+      @current_speed = DEFAULT_SPEED
     end
 
-    def speed_up(velocity)
-      @current_speed += velocity
+    def speed_up(more_speed)
+      @current_speed += more_speed
     end
 
-    def push_break(velocity)
-      @current_speed -= velocity if velocity <= @current_speed
+    def push_break(less_speed)
+      @current_speed -= less_speed if less_speed <= @current_speed
     end
   end
 end
